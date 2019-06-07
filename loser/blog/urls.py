dce084f38a04c1c6f318ca67f8171fc7b39d8223
Ocitkowski <o.citkowski@gmail.com>
2019-06-07 17:55:12 +0300

    go to home

--- loser/blog/urls.py
+++ loser/blog/urls.py
@@ -5,5 +5,8 @@ from  .views import *
 
 urlpatterns = [
 
-    path('', post_list)
+    path('', post_list, name='posts_list_urls'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tags_list, name='tags_list_url'),
+    path('tag/<str:slug>/', tag_detail, name='tag_detail_url'),
 ]
--- loser/blog/views.py
+++ loser/blog/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post, Tag
 
 def post_list(reguest):
-    names = ['Alex', 'Lara', '*****', 'Megi']
-    print(reguest)
-    return render(reguest, 'blog/blog.html', context={'names' : names})
+    # names = ['Alex', 'Lara', '*****', 'Megi']
+    # print(reguest)
+    # return render(reguest, 'blog/blog.html', context={'names' : names})
+    posts = Post.objects.all()
+    return render(reguest, 'blog/blog.html', context={'posts':posts} )
+
+def post_detail(reguest, slug):
+
+    post = Post.objects.get(slug__iexact=slug)
+    return render(reguest, 'blog/post_detail.html', context={'post':post} )
+
+def tag_detail(reguest, slug):
+
+    tag = Tag.objects.get(slug__iexact=slug)
+    return render(reguest, 'blog/tag_detail.html', context={'tag':tag} )
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags':tags})
